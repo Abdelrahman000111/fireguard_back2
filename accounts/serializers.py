c31b7ce9c4ec13edde833b327f1394ce0a46f748
abdelrahman <abdelrahmanreda00001@gmail.com>
2026-05-13 00:50:50 +0300

    Done

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -42,7 +42,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model  = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'profile']
         read_only_fields = ['username']
 
 
